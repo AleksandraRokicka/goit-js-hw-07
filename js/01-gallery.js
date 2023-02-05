82661dba2ea8b40085aa1c95ea603c1c4db5f1df
AleksandraRokicka <rokicka.ola@gmail.com>
2023-02-05 16:06:02 +0100

    escapeKey change

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,8 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
 const listGallery = document.querySelector(".gallery");
+
 galleryItems.forEach((img) => {
   listGallery.insertAdjacentHTML(
     "beforeend",
@@ -20,23 +22,23 @@ galleryItems.forEach((img) => {
 listGallery.addEventListener("click", modalImg);
 //podpięcie funkcji przez referencję do niej//
 function modalImg(event) {
-  console.log(event.target);
   event.preventDefault();
+  console.log(event.target);
   if (event.target.nodeName !== "IMG") {
     return;
   }
 
   const instance = basicLightbox.create(`
-<img src="${event.target.getAttribute("data-source")}">`);
+<img src="${event.target.dataset.source}" width="800" height="600">`);
   instance.show();
 
-  document.addEventListener("keydown", escapeKey);
-  function escapeKey(event) {
-    if (event.key === "Escape") {
+
+
+  document.addEventListener("keydown", (escapeKey) => {
+    if (escapeKey.key === "Escape") {
       instance.close();
-      document.removeEventListener("keydown", escapeKey);
       console.log("esc");
     }
-  }
+  });
 }
 console.log(galleryItems);
